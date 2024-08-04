80402df2e0371c974b7fe331bf1387072e5191f5
emojiiii <1207145565@qq.com>
2024-08-04 21:57:44 +0800

    Add KolorsMultiTextEncode node

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
-from .nodes.MultiTextEncode import MultiTextEncode
+from .nodes import MultiTextEncode, KolorsMultiTextEncode
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-    "MultiTextEncode": MultiTextEncode
+    "MultiTextEncode": MultiTextEncode,
+    "KolorsMultiTextEncode": KolorsMultiTextEncode
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "MultiTextEncode": "MultiTextEncode"
+    "MultiTextEncode": "MultiTextEncode",
+    "KolorsMultiTextEncode": "KolorsMultiTextEncode"
 }
